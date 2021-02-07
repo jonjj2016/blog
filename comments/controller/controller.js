6f88e58cc8ce302ab283dda4b01ec7c6bad5625c
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-02-08 01:33:45 +0800

    added get and create functionality to comments microservice

--- comments/controller/controller.js
+++ comments/controller/controller.js
@@ -1,7 +1,26 @@
-exports.get=(req,res)=>{
+const {commentsByPostId}=require('../utils/util');
+const {randomBytes}=require('crypto');
 
+
+exports.get=(req,res)=>{
+    const comments=commentsByPostId[req.params.id]||[];
+  
+    res.status(200).json({
+        status:true,
+        data:comments
+    })
 };
 
 exports.create=(req,res)=>{
-    
+    const id=randomBytes(4).toString('hex');
+    const {content}=req.body;
+    const comments=commentsByPostId[req.params.id]||[]
+    comments.push({
+        id,content
+    })
+    commentsByPostId[req.params.id]=comments;
+    res.status(201).json({
+        status:true,
+        data:comments,
+    });
 }
